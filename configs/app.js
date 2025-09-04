9ac207877d181bfa30f9d33ce413f27275fa8680
Ingrid Choc <ingridchoc@sanjudastadeo.edu.gt>
2025-09-04 15:55:30 -0600

    Refactor: configurando de manera correcta el servidor

---
+++ configs/app.js
@@ -0,0 +1,28 @@
+'user strict';
+
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+import 'dotenv/config';
+
+const middlewares = (app) => {
+    app.use(express.json());
+    app.use(express.urlencoded({extended: false}));
+    app.use(cors());
+    app.use(helmet());
+    app.use(morgan('dev'));
+}
+
+export const initServer = () => {
+    const app = express();
+    
+    try{
+        app.listen(process.env.PORT, () =>{
+            console.log (`Server runnig on port ${process.env.PORT}`)
+        })
+    }catch(error){
+        console.log(`Error al iniciar al servidor: ${error}`);
+    }
+}
