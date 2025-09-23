26f9e04093938beba12cc5731c0bb0e96bf568e0
Ingrid Choc <ingridchoc@sanjudastadeo.edu.gt>
2025-09-23 15:58:01 -0600

    23/09/2025

--- configs/app.js
+++ configs/app.js
@@ -6,6 +6,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import 'dotenv/config';
 import { dbConnection } from './db.js';
+import userModel from '../src/users/user.model.js';
 
 const middlewares = (app) => {
     app.use(express.json());
